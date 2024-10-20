f5b3ab7091cb2ca6b455434d65b2e3f1f7942ad2
Thomas Edward Little García-Huidobro <43427319+ThomasLittleGH@users.noreply.github.com>
2024-10-20 16:45:14 -0300

    Time: 54 ms (99.3%), Space: 18.2 MB (5.45%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,17 +1,19 @@
+import heapq
 class KthLargest(object):
 
     def __init__(self, k, nums):
-        """
-        :type k: int
-        :type nums: List[int]
-        """
-        
+        self.nums = []
+        self.k = k
+        if nums:
+            self.nums = heapq.nlargest(k, nums)
+            heapq.heapify(self.nums)
 
     def add(self, val):
-        """
-        :type val: int
-        :rtype: int
-        """
+        if self.k > len(self.nums):
+            heapq.heappush(self.nums, val)
+        else:
+            heapq.heappushpop(self.nums, val)
+        return self.nums[0]
         
 
 
